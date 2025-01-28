0f95916d4967d9151161f9fe0eeddebcebed657b
Raí Araujo <raisanhacu@gmail.com>
2025-01-27 21:43:38 -0300

    Criação da entidade Users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn} from 'typeorm';
+  
+  @Entity('users') 
+  export class User {
+    @PrimaryGeneratedColumn('uuid') 
+    id: string;
+  
+    @Column({ type: 'varchar', unique: true, length: 50 })
+    username: string;
+  
+    @Column({ type: 'varchar', unique: true, length: 100 })
+    email: string;
+  
+    @Column({ type: 'varchar' }) 
+    password: string;
+  
+    @Column({ type: 'varchar', nullable: true }) 
+    profile_picture?: string;
+  
+    @Column({ type: 'text', nullable: true }) 
+    bio?: string;
+  
+    @CreateDateColumn() 
+    created_at: Date;
+  
+    @UpdateDateColumn() // Atualiza automaticamente ao alterar
+    updated_at: Date;
+  }
+  
