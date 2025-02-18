e961844bf8b09498e1383c04fc14785a7e6cb755
Raí Araujo <raisanhacu@gmail.com>
2025-02-18 12:41:05 -0300

    ✨  feat: Conexão com o BD

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from 'src/users/entities/user.entity';
+import { Problem } from 'src/problems/entities/problem.entity';
+import { UserProgress } from 'src/user_progress/entities/user_progress.entity';
+import { Challenge } from 'src/challenges/entities/challenge.entity';
+import { Comment } from 'src/comments/entities/comment.entity';
+import { Notification } from 'src/notifications/entities/notification.entity';
+import { Post } from 'src/posts/entities/post.entity';
 
 @Module({
   imports: [
@@ -17,12 +24,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         username: configService.get<string>('DB_USERNAME', 'postgres'),
         password: configService.get<string>('DB_PASSWORD', 'postgres'),
         database: configService.get<string>('DB_DATABASE', 'devlingo'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'], // Carrega as entidades automaticamente
+        entities: [User, UserProgress, Problem, Challenge, Comment, Notification, Post], // Carrega as entidades automaticamente
         synchronize: configService.get<boolean>('DB_SYNCHRONIZE', true), // Apenas para desenvolvimento
         logging: configService.get<boolean>('DB_LOGGING', false),
         ssl: {
           rejectUnauthorized: false, 
         },
+        migrations: ['src/migrations/*.ts'],
       }),
     }),
   ],
